ae0474061512375f28fa7d3acf83d1a2a4ae5bb2
Richard Pitts <Richardepitts@gmail.com>
2019-09-19 18:39:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Write your solution in this file!
 const driver = {}
 
+//     This function should not mutate the driver and should return a 
+//     new driver that has an updated value for the key passed in.
+
 function updateDriverWithKeyAndValue(object, key, value) {
   // driver.assign(street: '11 Broadway')
   return Object.assign({}, object, {[key]: value});
@@ -22,4 +25,4 @@ function destructivelyDeleteFromDriverByKey(object, key){
   delete object[key]
   return object
 }
-// const newDriver = updateDriverWithKeyAndValue(driver, 'key', 'value')
+
