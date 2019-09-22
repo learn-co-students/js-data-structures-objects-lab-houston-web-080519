940976fc999996acc2c1b8f0dcfdbb7e4b734fde
Emily <mle.jo.11@gmail.com>
2019-09-22 10:39:43 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution in this file!
+const driver = {
+    name: 'Emily',
+};
+
+function updateDriverWithKeyAndValue(obj, key, value) {
+    const newObj = { ...obj };
+
+    newObj[key] = value;
+
+    return newObj;
+}
+
+function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+
+    return obj;
+}
+
+function deleteFromDriverByKey(obj, key) {
+    const newObj = { ...obj };
+    delete newObj[key];
+
+    return newObj;
+}
+
+function destructivelyDeleteFromDriverByKey(obj, key, value) {
+    delete obj[key];
+
+    return obj;
+}
